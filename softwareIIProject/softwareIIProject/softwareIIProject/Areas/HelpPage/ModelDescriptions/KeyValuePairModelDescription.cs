a188eee36f95823d05afc372e11036457d70c2fb
Abdelrhman199 <abdelrhman_hosny1212@yahoo.com>
2020-04-02 20:27:00 +0200

    change REgister type from get to post

--- softwareIIProject/softwareIIProject/softwareIIProject/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace softwareIIProject.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
